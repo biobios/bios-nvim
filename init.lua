45df146cceaba68808946011f970283dfa05779a
BioBIOS <biobios1024@gmail.com>
2025-05-18 14:02:05 +0900

    feat: initialize module with class and submode setup

--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
-vim.notify("Hello world from init.lua")
+local M = {}
+
+M.class = require("bios-nvim.class")
+-- M.user_operator = require("bios-nvim.user-operator")
+M.submode = require("bios-nvim.submode")
+
+function M.setup(self)
+    self = self or {}
+    -- self.user_operator = self.user_operator or M.user_operator
+    self.submode = self.submode or M.submode
+    self.class = self.class or M.class
+
+    self.class:setup()
+    -- self.user_operator:setup()
+    self.submode:setup()
+end
+
+return M
