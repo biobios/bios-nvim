583663b5b9943649f7f145cd3ad1ead636abeb31
BioBIOS <biobios1024@gmail.com>
2025-05-18 14:01:49 +0900

    feat: update submode class to use instance-specific IDs for key mappings

--- submode/init.lua
+++ submode/init.lua
@@ -5,21 +5,29 @@ local SID = vim.fn.expand("<SID>")
 local class = require("bios-nvim.class")
 
 M.setup = function(self)
-    self = self or {}
 end 
 
 local class_submode = class.def_class
 {
+    static = {
+        SID = vim.fn.expand("<SID>"),
+        ID = 0,
+        get_id = function(self)
+            self.ID = self.ID + 1
+            return self.SID .. self.ID .. "_"
+        end,
+    }
     methods = {
         initialize = function(self, name, enter_cmd, modes)
             self.name = name
             self.modes = modes
+            self.id = self.class:get_id()
             vim.keymap.set(modes, enter_cmd, function()
                 vim.opt.timeout = false
                 vim.notify("-- " .. name .. " --")
-                return SID .. name
+                return self.id .. name
             end, { script = true, noremap = true, nowait = true, expr = true })
-            vim.keymap.set(modes, SID .. name, self:get_leave_submode_with_cmd(""), { noremap = true, expr = true })
+            vim.keymap.set(modes, self.id .. name, self:get_leave_submode_with_cmd(""), { noremap = true, expr = true })
         end,
         add_command = function(self, lhs, rhs, opts)
             if not opts then
@@ -28,7 +36,7 @@ local class_submode = class.def_class
             if not opts.expr then
                 opts.expr = true
             end
-            vim.keymap.set(self.modes, SID .. self.name .. lhs, self:get_leave_submode_with_cmd(rhs), opts)
+            vim.keymap.set(self.modes, self.id .. self.name .. lhs, self:get_leave_submode_with_cmd(rhs), opts)
         end,
         get_leave_submode_with_cmd = function(self, cmd)
             return function()
